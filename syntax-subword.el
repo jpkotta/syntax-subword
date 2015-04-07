e28bdf7a63105acc0169eb2d343038532278d98c
Jonathan Kotta <jpkotta@gmail.com>
2015-04-07 16:19:32 -0500

    rename functions

--- syntax-subword.el
+++ syntax-subword.el
@@ -43,10 +43,6 @@
 ;; Stops on spaces can be eliminated by setting
 ;; `syntax-subword-skip-spaces' to non-nil.
 
-;; TODO
-
-;; some of these names (the "miscellaneous utilities") should be changed
-
 ;;; Code:
 
 
@@ -58,21 +54,21 @@
 
 ;; recommended keybindings:
 
-;; (global-set-key (kbd "C-k") 'syntax-subword-delete-line)
+;; (global-set-key (kbd "C-k") 'jpk-delete-line)
 ;; (global-set-key (kbd "C-S-k") 'kill-line)
 
 ;; (global-set-key (kbd "M-SPC") 'delete-horizontal-space)
 ;; (global-set-key (kbd "M-S-SPC") 'delete-blank-lines)
 
-;; (global-set-key (kbd "<C-S-delete>") 'syntax-subword-delete-syntax)
-;; (global-set-key (kbd "<C-S-backspace>") 'syntax-subword-backward-delete-syntax)
-;; (global-set-key (kbd "C-<delete>") 'syntax-subword-delete-word)
-;; (global-set-key (kbd "M-d") 'syntax-subword-delete-word)
-;; (global-set-key (kbd "C-<backspace>") 'syntax-subword-backward-delete-word)
+;; (global-set-key (kbd "<C-S-delete>") 'jpk-delete-syntax)
+;; (global-set-key (kbd "<C-S-backspace>") 'jpk-backward-delete-syntax)
+;; (global-set-key (kbd "C-<delete>") 'jpk-delete-word)
+;; (global-set-key (kbd "M-d") 'jpk-delete-word)
+;; (global-set-key (kbd "C-<backspace>") 'jpk-backward-delete-word)
 
 ;; from Jonathan Arkell (http://stackoverflow.com/questions/154097/whats-in-your-emacs/154980#154980)
 ;;;###autoload
-(defun syntax-subword-kill-syntax (&optional arg)
+(defun jpk-kill-syntax (&optional arg)
   "Kill ARG sets of syntax characters after point."
   (interactive "p")
   (let ((arg (or arg 1))
@@ -86,13 +82,13 @@
     (kill-region opoint (point))))
 
 ;;;###autoload
-(defun syntax-subword-kill-syntax-backward (&optional arg)
+(defun jpk-kill-syntax-backward (&optional arg)
   "Kill ARG sets of syntax characters preceding point."
   (interactive "p")
-  (syntax-subword-kill-syntax (- 0 (or arg 1))))
+  (jpk-kill-syntax (- 0 (or arg 1))))
 
 
-(defmacro syntax-subword-delete-instead-of-kill (&rest body)
+(defmacro jpk-delete-instead-of-kill (&rest body)
   "Replaces `kill-region' with `delete-region' in BODY."
   `(cl-letf (((symbol-function 'kill-region)
               (lambda (beg end &optional yank-handler)
@@ -100,39 +96,39 @@
      ,@body))
 
 ;;;###autoload
-(defun syntax-subword-delete-syntax (arg)
-  "Like `syntax-subword-kill-syntax', but does not save to the `kill-ring'."
+(defun jpk-delete-syntax (arg)
+  "Like `jpk-kill-syntax', but does not save to the `kill-ring'."
   (interactive "*p")
-  (syntax-subword-delete-instead-of-kill
-   (syntax-subword-kill-syntax arg)))
-(put 'syntax-subword-delete-syntax 'CUA 'move)
+  (jpk-delete-instead-of-kill
+   (jpk-kill-syntax arg)))
+(put 'jpk-delete-syntax 'CUA 'move)
 
 ;;;###autoload
-(defun syntax-subword-backward-delete-syntax (arg)
+(defun jpk-backward-delete-syntax (arg)
   "Like `syntax-subword-backward-kill-syntax', but does not save to the `kill-ring'."
   (interactive "*p")
-  (syntax-subword-delete-syntax (- arg)))
-(put 'syntax-subword-backward-delete-syntax 'CUA 'move)
+  (jpk-delete-syntax (- arg)))
+(put 'jpk-backward-delete-syntax 'CUA 'move)
 
 ;;;###autoload
-(defun syntax-subword-delete-word (arg)
+(defun jpk-delete-word (arg)
   "Like `kill-word', but does not save to the `kill-ring'."
   (interactive "*p")
-  (syntax-subword-delete-instead-of-kill (kill-word arg)))
-(put 'syntax-subword-delete-word 'CUA 'move)
+  (jpk-delete-instead-of-kill (kill-word arg)))
+(put 'jpk-delete-word 'CUA 'move)
 
 ;;;###autoload
-(defun syntax-subword-backward-delete-word (arg)
+(defun jpk-backward-delete-word (arg)
   "Like `backward-kill-word', but does not save to the `kill-ring'."
   (interactive "*p")
-  (syntax-subword-delete-word (- arg)))
-(put 'syntax-subword-backward-delete-word 'CUA 'move)
+  (jpk-delete-word (- arg)))
+(put 'jpk-backward-delete-word 'CUA 'move)
 
 ;;;###autoload
-(defun syntax-subword-delete-line (&optional arg)
+(defun jpk-delete-line (&optional arg)
   "Like `kill-line', but does not save to the `kill-ring'."
   (interactive "*P")
-  (syntax-subword-delete-instead-of-kill (kill-line arg)))
+  (jpk-delete-instead-of-kill (kill-line arg)))
 
 
 ;;; syntax-subword
@@ -143,19 +139,19 @@
 (defvar syntax-subword-mode-map
   (let ((map (make-sparse-keymap)))
     (dolist (old-and-new
-             '((right-word           syntax-subword-right)
-               (left-word            syntax-subword-left)
-               (forward-word         syntax-subword-forward)
-               (backward-word        syntax-subword-backward)
-               (mark-word            syntax-subword-mark)
-               (kill-word            syntax-subword-kill)
-               (backward-kill-word   syntax-subword-backward-kill)
-               (syntax-subword-delete-word syntax-subword-delete)
-               (syntax-subword-backward-delete-word syntax-subword-backward-delete)
-               (transpose-words      syntax-subword-transpose)
-               (capitalize-word      syntax-subword-capitalize)
-               (upcase-word          syntax-subword-upcase)
-               (downcase-word        syntax-subword-downcase)))
+             '((right-word         syntax-subword-right)
+               (left-word          syntax-subword-left)
+               (forward-word       syntax-subword-forward)
+               (backward-word      syntax-subword-backward)
+               (mark-word          syntax-subword-mark)
+               (kill-word          syntax-subword-kill)
+               (backward-kill-word syntax-subword-backward-kill)
+               (jpk-delete-word    syntax-subword-delete)
+               (jpk-backward-delete-word syntax-subword-backward-delete)
+               (transpose-words    syntax-subword-transpose)
+               (capitalize-word    syntax-subword-capitalize)
+               (upcase-word        syntax-subword-upcase)
+               (downcase-word      syntax-subword-downcase)))
       (let ((oldcmd (car old-and-new))
             (newcmd (cadr old-and-new)))
         (define-key map (vector 'remap oldcmd) newcmd)))
@@ -216,7 +212,7 @@ subword (see `subword-mode' for a description of subwords)."
 
 (defun syntax-subword-delete (&optional n)
   (interactive "^p")
-  (syntax-subword-delete-instead-of-kill (syntax-subword-kill n)))
+  (jpk-delete-instead-of-kill (syntax-subword-kill n)))
 
 (defun syntax-subword-backward-kill (&optional n)
   (interactive "^p")
@@ -224,7 +220,7 @@ subword (see `subword-mode' for a description of subwords)."
 
 (defun syntax-subword-backward-delete (&optional n)
   (interactive "^p")
-  (syntax-subword-delete-instead-of-kill (syntax-subword-backward-kill n)))
+  (jpk-delete-instead-of-kill (syntax-subword-backward-kill n)))
 
 (defalias 'syntax-subword-mark 'subword-mark)
 (defalias 'syntax-subword-transpose 'subword-transpose)
