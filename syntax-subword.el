02e7f4dee1ca9dc32af6a06398c5b6cd235b633d
rblack <unknown>
2016-02-04 10:59:43 -0700

    adding 'consistent' special value for syntax-word-skip-spaces, which always stops at right edge of spaces.

--- syntax-subword.el
+++ syntax-subword.el
@@ -136,7 +136,9 @@
 ;;; syntax-subword
 
 (defvar syntax-subword-skip-spaces nil
-  "When non-nil, do not stop on spaces.")
+  "When non-nil, do not stop on spaces.  
+When set to the special symbol 'consistent, stop at right edge of spaces 
+regardless of direction.")
 
 (defvar syntax-subword-mode-map
   (let ((map (make-sparse-keymap)))
@@ -276,10 +278,12 @@ Negative COUNT moves backwards."
       (if (< 0 sign)
           (goto-char (max (1+ (point)) (min subword-pos syntax-pos)))
         (goto-char (min (1- (point)) (max subword-pos syntax-pos))))
-      (unless (and syntax-subword-skip-spaces
-                 (or 
-                  (and (< 0 sign) (looking-at "[[:space:]]"))
-                  (and (> 0 sign) (looking-back "[[:space:]]"))))
+      (unless (cond ((eq syntax-subword-skip-spaces 'consistent)
+                     (looking-at "[[:space:]]"))
+                    (syntax-subword-skip-spaces
+                     (or 
+                      (and (< 0 sign) (looking-at "[[:space:]]"))
+                      (and (> 0 sign) (looking-back "[[:space:]]")))))
         (setq count (1- count)))
       )))
 
